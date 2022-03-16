3cb520b9ff49288084e52234fb171050043609e6
Thomas Breno Bertelli Mendes <bertellimendes@gmail.com>
2022-03-16 20:53:59 -0300

    ADD HTML, JS e criação do array de objetos

---
+++ main.js
@@ -0,0 +1,34 @@
+let inputTitulo = document.querySelector('#titulo')
+let inputDescricao = document.querySelector('#descricao')
+let inputImagem = document.querySelector('#imagem')
+let botaoEnviar = document.querySelector('#enviar')
+
+let cardsRefence = document.querySelector('#cards')
+
+let posts = [
+]
+
+botaoEnviar.addEventListener('click',function(event){
+    
+    let inputTituloAtual = document.querySelector('#titulo').value
+    let inputDescricaoAtual = document.querySelector('#descricao').value
+    let inputImagemAtual = document.querySelector('#imagem').value
+
+    posts.push ({
+        titulo: inputTituloAtual,
+        descricao: inputDescricaoAtual,
+        imagem: inputImagemAtual
+    })
+    event.preventDefault()
+    console.log(posts)
+})
+
+// for(let post of posts){
+//     conteinerReference.innerHTML+=`
+//     <div class="item">
+//             <img src="${post.imagem}">
+//             <h2> ${post.titulo}</h2>
+//             <p>${post.descricao}</p>
+//         </div>
+//     `
+// }
